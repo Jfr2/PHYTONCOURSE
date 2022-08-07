fe79d95fa0f3d2807cb3ac2265836f50e09e93da
Jfr2 <110782036+Jfr2@users.noreply.github.com>
2022-08-07 15:12:51 -0500

    Create introduccion.py

---
+++ introduccion.py
@@ -0,0 +1,12 @@
+# -*- codi
+nombre= input ("escribe el nombre de un libro")
+print ("el nombre del libro es:", nombre )
+autor= input("escribre el nombre del autor")
+print ("el autor del libro es", autor )
+if autor=="gabo":
+    print ("el autor es gabo")
+if autor=="gabriel":
+    print (autor)
+else :
+    print ("el autor no coincide")
+    
